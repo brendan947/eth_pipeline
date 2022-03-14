1dc736f7aaf9a1f498a4abff5e995c3c57b8d965
brendan <brendan.woulfe@gmail.com>
2022-03-14 15:03:41 -0400

    2nd tutorial

---
+++ tutorial_2.py
@@ -0,0 +1,18 @@
+import pendulum
+from airflow.operators.python_operator import PythonOperator
+from airflow import DAG
+
+with DAG(
+    dag_id = 'eth_etl',
+    start_date=pendulum.datetime(2022, 1, 1, tz="UTC"),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    @task(task_id="testing_python_operator")
+    def test_function(**kwargs):
+        print(kwargs)
+
+        return 'Return test function to logs'
+
+    run_this = test_function()
