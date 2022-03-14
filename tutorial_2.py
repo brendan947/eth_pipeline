18f886b1484dd591192f7ffd00cdde04931b77bf
brendan <brendan.woulfe@gmail.com>
2022-03-14 16:11:44 -0400

    add dag tasks

--- tutorial_2.py
+++ tutorial_2.py
@@ -1,6 +1,13 @@
 import pendulum
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow import DAG
+
+
+def test_function(**kwargs):
+    print(kwargs)
+
+    return 'Return test function to logs'
 
 with DAG(
     dag_id = 'eth_etl',
@@ -8,11 +15,7 @@ with DAG(
     schedule_interval="@daily",
     catchup=False
 ) as dag:
+    dummy_task = DummyOperator(task_id='dummy_task', retries=3)
+    extract_task = PythonOperator(task_id="extract_task",python_callable=test_function("testing python task"))
 
-    @task(task_id="testing_python_operator")
-    def test_function(**kwargs):
-        print(kwargs)
-
-        return 'Return test function to logs'
-
-    run_this = test_function()
+    dummy_task >> extract_task
